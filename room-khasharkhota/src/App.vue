5bc09243dcbdb6c6e6bf02309bc222eaa80990fa
Akhmed <ahmedmalaciev4@gmail.com>
2025-10-28 19:59:11 +0300

    Создал проект, подключил шрифты и проверил корректную работу Start.vue

---
+++ room-khasharkhota/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <Start />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import Start from './sections/start.vue'
+</script>
+<style scoped lang="scss">
+
+</style>
---
+++ room-khasharkhota/src/sections/Start.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="start">
+    Hello world
+  </section>
+</template>
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+  .start {
+  }
+</style>
